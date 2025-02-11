3c6b9982037b7175049d0454ca4e7f9d0a2c62e5
Vaidushya <anugya2014@gmail.com>
2025-02-11 12:20:02 +0530

    my python

---
+++ main.py
@@ -0,0 +1,17 @@
+# greeting
+print("Hi there")
+
+# my name
+print("My name is Vaidushya")
+
+# school name
+print("My school's name is SIS school")
+
+# i love coding
+print("I 💗 coding")
+
+# my teacher
+print("My teacher's name is mr. Sayan Mitra")
+
+# about my classes
+print("I love to study coding")
